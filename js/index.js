69448a3195a29d3625dab64083ad118706c537fe
YuichiSemura <semura.yuichi@gmail.com>
2020-04-02 02:11:30 +0900

    [add] top page story

--- js/index.js
+++ js/index.js
@@ -2,4 +2,23 @@ init()
 
 function init() {
   console.log("init()")
+
+  var animation = 'rubberBand'
+
+  $('a').hover(function(event) {
+      event.target.classList.add('animated');
+      event.target.classList.add(animation);
+    },
+    function(event) {
+      event.target.classList.remove('animated');
+      event.target.classList.remove(animation);
+    }
+  );
 }
+wow = new WOW({
+  boxClass: 'wow', // default
+  animateClass: 'animated', // default
+  offset: 0 // アニメーションをスタートさせる距離
+});
+
+wow.init();
